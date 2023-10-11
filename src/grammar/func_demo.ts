b1eae319fb403085e3d6dd72c6987a4e529666f5
yehot <yehot2013@gmail.com>
2023-10-12 00:30:15 +0800

    泛型、enum

---
+++ src/grammar/func_demo.ts
@@ -0,0 +1,15 @@
+
+// 泛型 T, P
+function test<T, P>(first: T, second: P): string {
+  return `${first} - ${second}`;
+}
+
+interface Item {
+  name: string,
+}
+
+// 泛型 T extends
+class Data<T extends Item> {
+  constructor(private data: T) {
+  }
+}
