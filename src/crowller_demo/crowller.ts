8dbb5ce1b03a011aa30fbd377b4aa95a168b3d9f
yehot <yehot2013@gmail.com>
2023-10-11 23:34:19 +0800

    cheerio: jquery 类似；superagent 请求网页

--- src/crowller_demo/crowller.ts
+++ src/crowller_demo/crowller.ts
@@ -1,16 +1,25 @@
 import superagent from 'superagent'
+import * as cheerio from "cheerio";
 
 class Crawler {
   private secret = 'secretKey';
   private url = '';
   
-  private rawHtml = '';
   
   async getRawHtml() {
     const result = await superagent.get(this.url);
     console.log(result.text);
     
+    this.getInfo(result.text);
   }
+  
+  // cheerio: 使用 jquery 的语法
+  getInfo(html: string) {
+    const $ = cheerio.load(html);
+    const item = $('.course-item');
+    console.log(item.length);
+  }
+  
   
   constructor() {
     this.getRawHtml().then(() => {
